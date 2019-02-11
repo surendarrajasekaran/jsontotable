fe3ee390a937a61d213108a49ba5de8a41a722e6
surendarrajasekaran <35868093+surendarrajasekaran@users.noreply.github.com>
2019-02-11 14:07:36 +0530

    Update Run.py

--- Final/Run.py
+++ Final/Run.py
@@ -1,11 +1,11 @@
-
 import json
 import unicodedata
 flist=['Format.json']
 l=[]
+
 for col in flist:
   result=""
-  html=""
+  html="<table class='table'>"
   exp=0
   act=0
   f = open(col,'r')
@@ -13,45 +13,47 @@ for col in flist:
   json_data = json.loads(ip)
   test_data = json_data['Test_Data']
   key_set = test_data[0].keys()
-  for i in test_data[0].values():
-    l.append(i)
-  exp=l[1]
-  act=l[2]
-  if(exp==act):
-    result= True
-    r="pass"
-  
-  else:
-    result=False
-    r="Fail"
-  
-  html="<table class='table'><tr>"
-  total_rec = len(test_data)
   for key in key_set:
     html += "<th>"+key+"</th>"
-  html+="<th>""results""<th>"
-  html+="<th>""ColorCode""<th>"
-  html+="</tr>"
-
-  html+="<tr id='somerow'>"
-  j=0
-  for i in range (0, len(test_data)):
-    for key in key_set:
-      
-      html+="<td id ='"+str(j)+"'>"+str(test_data[i][key])+"</td>"
-      j=j+1
-  html+="<td>"+r+"</td>"
-  if(result==False and r=='Fail'):
-    html+="<td bgcolor='black'>""</td>"
-  else:
-    html+="<td bgcolor='pink'>""</td>"
-  html+="</tr>"
-  
-
-  html+="</table>"
-  html+="<div id='piechart'></div>"
-  print(html)
+  html+="<th>""Results""</th>"
+  html+="<th>""Status""</th>"
+  for ij in range(0,len(test_data)):
+      l.append(test_data[ij].values())
+  for i in l:
+    html+="<tr>"
+    
+    co=0
+    id=0
+    for j in i:
+      html+="<td id='"+str(id)+"'>"+str(j)+"</td>"
+      id=id+1
+      co=co+1
+      if(co==2):
+        exp=j
+      elif(co==3):
+        act=j
+    print(exp,act)
+    if(exp==act):
+      print("Pass")
+      result="Pass"
+    else:
+      result="Fail"
+    html+="<td>"+str(result)+"</td>"
+    if(result=="Pass"):
+      html+="<td bgcolor='Green'>""</td>"
+    elif(result=='Fail'):
+      html+="<td bgcolor='Red'>""</td>"
+      html+="</tr>"
   with open("index.html", "a") as myfile:
-      myfile.write(str(html))
-
-
+    myfile.write(str(html))
+html1="</table>"
+html1+="<div id='piechart'></div>"
+with open("index.html", "a") as myfile:
+    myfile.write(str(html1))
+      
+print(html)
+  
+      
+      
+      
+      
